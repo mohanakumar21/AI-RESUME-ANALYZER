b34f5f2c39a7909c8b9f00c34704c952e2f8e783
Mohanakumar S <Mohanakumar2106@gmail.com>
2026-07-29 13:41:20 +0530

    Sprint 1: Implemented resume upload system

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
-
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -10,17 +9,44 @@ app = Flask(
     template_folder=str(BASE_DIR / "templates"),
     static_folder=str(BASE_DIR / "static")
 )
-print("Root Path:", app.root_path)
-print("Template Folder:", app.template_folder)
-print("Template Exists:", (BASE_DIR / "templates" / "index.html").exists())
 
-print("Current Working Directory:", os.getcwd())
-print("App Root:", app.root_path)
-print("Template Folder:", os.path.join(app.root_path, "templates"))
+UPLOAD_FOLDER = BASE_DIR / "uploads"
+UPLOAD_FOLDER.mkdir(exist_ok=True)
+
+app.config["UPLOAD_FOLDER"] = str(UPLOAD_FOLDER)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+
+    if "resume" not in request.files:
+        return "No file uploaded."
+
+    file = request.files["resume"]
+
+    if file.filename == "":
+        return "No file selected."
+
+    filepath = os.path.join(
+        app.config["UPLOAD_FOLDER"],
+        file.filename
+    )
+
+    file.save(filepath)
+
+    return f"""
+    <h1>✅ Upload Successful</h1>
+
+    <h3>{file.filename}</h3>
+
+    <a href="/">Go Back</a>
+    """
+
 
 if __name__ == "__main__":
     app.run(debug=True)
